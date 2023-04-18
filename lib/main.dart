e2e3253deac44184ecc371e4982c7c1abb60390b
Thirumurugan Arumugam <Thirumurugan.A@avasoft.com>
2023-04-18 13:57:26 +0530

    third commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
-import 'package:apiintegration/Services/AvengerServices/MockAvengerService.dart';
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
 import 'App.dart';
-import 'BO/AvengerBO/AvengerBO.mapper.g.dart';
+import 'BO/AvengerBo/AvengerBO.mapper.g.dart';
 import 'Services/AvengerServices/AvengerService.dart';
 import 'Services/AvengerServices/IAvengerService.dart';
 import 'main.reflectable.dart';
